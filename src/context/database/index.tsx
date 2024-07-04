260d36f785150b41c995017370be069e4b1b1604
Gianluca Sampogna <g.sampogna@quattoror.com.br>
2024-07-04 14:14:17 -0300

    fix: database corrigido

--- src/context/database/index.tsx
+++ src/context/database/index.tsx
@@ -1,31 +1,20 @@
-import React, { createContext, useState, ReactNode, useEffect } from "react";
-import axios from "axios";
-import { doc, setDoc, collection, getDoc, DocumentData, DocumentSnapshot, getDocs, QuerySnapshot } from "firebase/firestore"; 
-
+import React, { createContext, useState, ReactNode } from "react";
+import { doc, setDoc, collection, getDocs, DocumentData } from "firebase/firestore"; 
 import db from '../../firebaseConnection';
 
 type AuthContextType = {
   auth: boolean;
   setAuth: React.Dispatch<React.SetStateAction<boolean>>;
-  addPedido: (name: string, faccao: string) => Promise<void>;
+  addPedido: (name: string, faccao: string | null) => Promise<void>;
   listaPedidos: () => Promise<DocumentData[]>;
 };
-
-interface IAddPedido{
-  name: string,
-  faccao: string | null,
-}
 
 export const DatabaseContext = createContext<AuthContextType>({} as AuthContextType);
 
 export const DatabaseProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [auth, setAuth] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   console.log(db ? 'Banco de dados funcionando' : 'Banco de dados não funcionando');
-  // }, []); // Adicionando um array de dependências vazio para que useEffect seja chamado apenas uma vez
-
-  const addPedido = async ({name, faccao}: IAddPedido): Promise<void> => {
+  const addPedido = async (name: string, faccao: string | null): Promise<void> => {
     const citiesRef = collection(db, "Pedidos");
     await setDoc(doc(citiesRef, name), {
       Nome: name, 
